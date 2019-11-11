d192e14f94033b1571976853fc3cbeb247fb55e0
AllenChen <15156027013@163.com>
2019-11-12 01:20:19 +0800

    上传提交

--- shopno7-upload/src/main/java/com/shopno7/ShopNo7UploadApplication.java
+++ shopno7-upload/src/main/java/com/shopno7/ShopNo7UploadApplication.java
@@ -1,3 +1,9 @@
+package com.shopno7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @ClassName ShopNo7UploadApplication
  * @Description TODO
@@ -5,5 +11,11 @@
  * @Date 2019/11/12 1:14
  * @Version 1.0
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ShopNo7UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNo7UploadApplication.class, args);
+    }
 }
